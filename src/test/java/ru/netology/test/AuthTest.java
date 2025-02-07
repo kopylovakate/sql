de699568d6b5ed1bf7acc0322da1cf75d90ad03d
katana <klimenko.katrin888@mail.ru>
2025-02-07 19:02:24 +0300

    init

---
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -0,0 +1,49 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import ru.netology.web.data.DataHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.SQLHelper;
+import ru.netology.web.page.LoginPage;
+
+public class AuthTest {
+    private DataHelper.AuthInfo authInfo;
+
+    @BeforeEach
+    public void setUp() {
+        Selenide.open("http://localhost:9999");
+
+        authInfo = DataHelper.getAuthInfo();
+    }
+
+    @Test
+    public void shouldSuccessAuth() {
+        var loginPage = new LoginPage();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.validVerify(authInfo);
+    }
+
+    @Test
+    public void shouldFailedAfterThreeBadInputCode() {
+        var loginPage = new LoginPage();
+        var verificationPage = loginPage.validLogin(authInfo);
+
+        String badCode = DataHelper.getBadCode().getCode();
+
+        verificationPage.invalidVerify(badCode);
+        verificationPage.findError();
+
+        verificationPage.invalidVerify(badCode);
+        verificationPage.findError();
+
+        verificationPage.invalidVerify(badCode);
+        verificationPage.findError();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        SQLHelper.deleteTestData();
+    }
+}
